321a0f2a31ce6264934d11bb76ca8b94d6205fea
Sai <saikrishna321@yahoo.com>
2019-11-22 13:37:30 +0000

    taiko and electron sample repo

---
+++ __tests__/taiko.spec.js
@@ -0,0 +1,33 @@
+const electron = require('electron');
+const kill = require('tree-kill');
+const tk = require('taiko');
+const { spawn } = require('child_process');
+
+const port = 9200; // Debugging port
+const timeout = 20000; // Timeout in miliseconds
+let pid;
+
+jest.setTimeout(timeout);
+
+beforeEach(async () => {
+
+  // Start Electron with custom debugging port
+  pid = spawn(electron, ['.', `--remote-debugging-port=${port}`], {
+    shell: true
+  }).pid;
+  console.log(process.versions['chrome'])
+  await tk.openBrowser({ headless: false, host: 'localhost', port });
+});
+
+afterEach(async () => {
+  try {
+    await tk.closeBrowser();
+  } catch (error) {
+    kill(pid);
+  }
+});
+
+  test('Text matches', async () => {
+    const isText = await tk.text('Demo of Electron Taiko.').exists();
+    console.log(isText);
+  });
